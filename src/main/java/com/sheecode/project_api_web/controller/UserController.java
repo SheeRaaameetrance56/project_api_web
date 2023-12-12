23c0e45c5ceb075bce4f6c08af7c80aaa5ec6fc0
Shehan wijeratne <shehanranjukaw5@gmail.com>
2023-12-12 19:03:10 +0530

    function UserController

--- src/main/java/com/sheecode/project_api_web/controller/UserController.java
+++ src/main/java/com/sheecode/project_api_web/controller/UserController.java
@@ -1,4 +1,21 @@
 package com.sheecode.project_api_web.controller;
 
+import com.sheecode.project_api_web.model.User;
+import com.sheecode.project_api_web.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserRepo userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
 }
